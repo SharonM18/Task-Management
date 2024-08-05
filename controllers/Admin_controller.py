888843a794a621930b15f9a6785061333c23c209
SharonM18 <sharonmashimbye74@gmail.com>
2024-08-05 12:06:05 +0200

    admin signup

---
+++ controllers/Admin_controller.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, url_for, session, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from bson.objectid import ObjectId
+from ..models.user import Newuser
+
+def signup():
+    name = request.json.get('name')
+    surname = request.json.get('surname')
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not all([name, surname, email, password]):
+        return jsonify({"error": "All fields are required"}), 400
+
+    hashed_password = generate_password_hash(password)
+    admin = {"name": name, "surname": surname, "email": email, "password": hashed_password}
+    
+    if Newuser.create_user( admin):
+        return jsonify({"message": "User created successfully", "redirect": url_for('admin.login')}), 201
+    else:
+        return jsonify({"error": "User creation failed"}), 500
+
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not all([email, password]):
+        return jsonify({"error": "Email and password are required"}), 400
+
+    admin = Newuser.get_user_by_email(email)
+    
+    if admin and check_password_hash(admin['password'], password):
+        # Assuming session management is done here, and user_id is stored in the session
+        session['user_id'] = str(admin['_id'])
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        return jsonify({"error": "Invalid email or password"}), 401
